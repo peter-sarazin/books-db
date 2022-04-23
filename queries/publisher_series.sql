7c0af9e0b9665afc43244fb3885d2fad8b183689
Peter Sarazin <peter.sarazin@gmail.com>
2022-04-22 23:42:34 -0400

    added book.sql

--- queries/publisher_series.sql
+++
@@ -1,8 +0,0 @@
-select p.publisher_name, s.series_name from publisher_series as ps
-left outer join publisher as p ON ps.publisher_id = p.publisher_id
-left outer join series as s ON ps.series_id = s.series_id;
-
-select s.series_name from publisher_series as ps
-left join publisher as p ON ps.publisher_id = p.publisher_id
-left join series as s ON ps.series_id = s.series_id
-where p.publisher_id=1;
